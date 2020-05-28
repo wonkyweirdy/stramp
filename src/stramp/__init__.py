52d9bbab356049da506cb4941715864d8bbd997e
Ray Burr <ryb@nightmare.com>
2020-05-28 18:39:43 -0400

    Issues with python-package workflow

--- src/stramp/__init__.py
+++ src/stramp/__init__.py
@@ -1,8 +1,12 @@
 
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib import metadata as importlib_metadata
+except ModuleNotFoundError:
+    # Backport for
+    import importlib_metadata
 
 try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:
     # package is not installed
     pass
