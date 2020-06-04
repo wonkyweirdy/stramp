1d227d04b40c78482b9f4311d951c99ac7152fd7
Ray Burr <ryb@nightmare.com>
2020-06-04 17:23:51 -0400

    Accept "commonmark" as a file format
    in addition to "markdown".

--- src/stramp/hashing.py
+++ src/stramp/hashing.py
@@ -64,7 +64,7 @@ def hash_files(docs: Iterable[DocFile], out: TextIO):
         if doc.file_format == 'org':
             from stramp.parsers.org_parser import load_file as load_org_file
             load_org_file(doc)
-        elif doc.file_format == 'markdown':
+        elif doc.file_format in ('commonmark', 'markdown'):
             from stramp.parsers.markdown_parser import load_file as load_markdown_file
             load_markdown_file(doc)
         else:
